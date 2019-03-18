55eeae217912e50df5e8e8aeba1af70b0a889670
VivenZZZ <33240607+VivenZZZ@users.noreply.github.com>
2019-03-18 17:28:35 +0800

    weui.wxss添加

--- pages/personal/personal.js
+++ pages/personal/personal.js
@@ -9,4 +9,19 @@ Page({
     wx.stopPullDownRefresh();
     console.log('修改数据')
   },
+  showInfos() {
+    console.log(111)
+    wx.getUserInfo({
+      success(res) {
+        const userInfo = res.userInfo
+        const nickName = userInfo.nickName
+        const avatarUrl = userInfo.avatarUrl
+        const gender = userInfo.gender // 性别 0：未知、1：男、2：女
+        const province = userInfo.province
+        const city = userInfo.city
+        const country = userInfo.country
+        console.log(res)
+      }
+    })
+  }
 })
